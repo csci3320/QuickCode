2535781261ac3808461533aced0dc91e974a4ba2
Ricks <bricks@unomaha.edu>
2022-10-11 13:39:03 -0500

    Create RecursiveList.py

---
+++ RecursiveList.py
@@ -0,0 +1,26 @@
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+class LinkedList:
+  def __init__(self):
+    self.root = None
+  
+  def add(self, value):
+    pass
+    return self
+  
+  def addRecursive(self, node, value):
+    pass
+    return node
+
+  def __str__(self):
+    return ""
+
+  def strRecursive(self, node):
+    pass
+    return ""
+
+print(LinkedList().add(1).add(2).add(3))
+    
